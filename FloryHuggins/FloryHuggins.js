6e7d082b0ca18710a821247af3d2d09e2881ca61
xianyang chen <owen__spy@outlook.com>
2022-09-15 14:47:09 -0500

    raw set up

---
+++ FloryHuggins/FloryHuggins.js
@@ -0,0 +1,9 @@
+function updateNaValue(){
+    console.log(document.getElementById("sliderA").value)
+}
+function updateNbValue(){
+    console.log(document.getElementById("sliderB").value);
+}
+function updateChiValue(){
+    console.log(document.getElementById("sliderchi").value);
+}
